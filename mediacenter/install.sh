c7f3fc747e4b295fd58988193a3bdf8c27363a8c
Sean Olson <seandolson654@gmail.com>
2018-08-12 16:02:54 -0700

    1

--- mediacenter/install.sh
+++ mediacenter/install.sh
@@ -5,8 +5,7 @@ echo 'deb https://dev2day.de/pms/ stretch main' > /etc/apt/sources.list.d/pms.li
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
 wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/stretch.list
 apt-get update
-apt -y install ./pkg/easy-x11.deb
-apt-get -y install udevil kodi plexmediaserver-installer git mopidy python-pip python-lxml bluealsa python-dbus
+apt-get -y install udevil plexmediaserver-installer mopidy python-pip python-lxml bluealsa python-dbus
 python -m pip install Mopidy-Iris mopidy-gmusic
 rm -r /usr/lib/python2.7/dist-packages/pyasn1
 python -m pip install pyasn1-modules
