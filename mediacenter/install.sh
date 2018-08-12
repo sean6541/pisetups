a0985eb8dec0d0a2e6da0b6711274c5ddbe7750f
Sean Olson <seandolson654@gmail.com>
2018-08-12 14:22:59 -0700

    1

--- mediacenter/install.sh
+++ mediacenter/install.sh
@@ -5,23 +5,9 @@ echo 'deb https://dev2day.de/pms/ stretch main' > /etc/apt/sources.list.d/pms.li
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
 wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/stretch.list
 apt-get update
-apt -y install ./deb/easy-x11.deb
-apt-get -y install udevil kodi plexmediaserver-installer git mopidy bluealsa python-dbus
-cd ./tmp
-git clone --depth 1 https://github.com/mopidy/mopidy-gmusic
-cd ./mopidy-gmusic
-python setup.py install
-chmod -R 755 /usr/local/lib/python2.7/dist-packages/gmusicapi-*-py2.7.egg/EGG-INFO/*
-cd ../
-rm -r ./mopidy-gmusic
-cd ../
-cd ./tmp
-git clone --depth 1 https://github.com/jaedb/Iris
-cd ./Iris
-python setup.py install
-cd ../
-rm -r ./Iris
-cd ../
+apt -y install ./pkg/easy-x11.deb
+apt-get -y install udevil kodi plexmediaserver-installer git mopidy python-pip python-lxml bluealsa python-dbus
+python -m pip install Mopidy-Iris mopidy-gmusic
 cp -r ./data/* /
 chmod +x /usr/local/bin/a2dp-agent
 sed -i 's/# #DiscoverableTimeout = 0/DiscoverableTimeout = 0/' /etc/bluetooth/main.conf
